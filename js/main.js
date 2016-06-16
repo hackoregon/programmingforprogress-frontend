e5f598f25862b109eccc63680e619bb639198a83
Merritt Lawrenson <merrittlawrenson@gmail.com>
2016-06-16 01:34:18 +0000

    whatever

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 // $('.js-transition-page').on('click', function(){
 //   $(this).removeClass('slide-in').addClass('slide-out');
 // });
+
+
+// Toggle read more or read less when stuff is shown or collapsed
+
+$('.story-collapse').on('shown.bs.collapse', function () {
+  $(this).next("a").html("<p><i>Read Less...</i></p>");
+});
+
+$('.story-collapse').on('hidden.bs.collapse', function () {
+  $(this).next("a").html("<p><i>Read More...</i></p>");
+});
