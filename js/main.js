91ad0623df8d7db24df95cfca55c51e8384dddd4
Merritt Lawrenson <merrittlawrenson@gmail.com>
2016-06-16 02:40:55 +0000

    commented out old cooooode

--- js/main.js
+++ js/main.js
@@ -5,13 +5,13 @@
 
 // Toggle read more or read less when stuff is shown or collapsed
 
-$('.story-collapse').on('shown.bs.collapse', function () {
-  $(this).next("a").html("<p><i>Read Less...</i></p>");
-});
+// $('.story-collapse').on('shown.bs.collapse', function () {
+//   $(this).next("a").html("<p><i>Read Less...</i></p>");
+// });
 
-$('.story-collapse').on('hidden.bs.collapse', function () {
-  $(this).next("a").html("<p><i>Read More...</i></p>");
-});
+// $('.story-collapse').on('hidden.bs.collapse', function () {
+//   $(this).next("a").html("<p><i>Read More...</i></p>");
+// });
 
 // Turn on the accordian
 
